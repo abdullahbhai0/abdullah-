6879437a87a4fe15c16eef45431688215c33812b
abdullahbhai0 <156930952+abdullahbhai0@users.noreply.github.com>
2024-01-18 22:01:54 +0500

    Add files via upload

---
+++ abdullah_midterm.py
@@ -0,0 +1,14 @@
+#abdullah
+#midterm
+#Q1 - Write a Python program to do arithmetical operations addition and division.
+
+print("the first number:",num1)p
+num2=6
+print ("the secont number")
+print("the sum of num1,2:", num1+num2)
+num3=25
+print("the divided for division",num3)
+num4=5
+print("the divided for division",num4)
+print("the answer:,num3 num4)
+      
